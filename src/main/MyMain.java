e7cab7f1572ff76b5fdd0a24ba6d252ae7792428
Astha Garg <astha.garg_cs.aiml19@gla.ac.in>
2021-04-13 23:31:07 +0530

    Insert method completed.

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -2,7 +2,6 @@ package main;
 
 public class MyMain {
     public static void main(String[] args) {
-        //create binary search tree
-        // and perform insert, search, traversal, delete
+
     }
 }
